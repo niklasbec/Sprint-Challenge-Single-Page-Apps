4831ba65f3b6201efc500d66bc3f1ac2caa034fe
niklasbec <niklasbeckerr@gmail.com>
2019-11-15 09:34:11 +0100

    API and router done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from './components/CharacterList'
+import { Route, Link } from 'react-router-dom'
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/' component={WelcomePage} />
+      <Route exact path='/characters' component={ CharacterList } />
     </main>
   );
 }
